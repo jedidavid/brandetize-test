7323479148db5260e5bca8354e2566d81e62dfe6
jedidavid <jeremydb.david@gmail.com>
2024-08-07 09:51:30 +0800

    add: ongoing homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#4b4334",
+        secondary: "#7e7666",
+        "red-orpiment": "#cb6f56",
+        lox: "#e99077",
+      },
+    },
     container: {
       padding: "1rem",
+    },
+    fontFamily: {
+      display: ["Playfair Display", "serif"],
+      body: ["Oxygen", "sans-serif"],
     },
   },
   plugins: [],
